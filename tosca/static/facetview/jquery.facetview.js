9d0ddf36a2112ff28d9f65f98c6ad03c70f19336
shitong01 <stchin@ntu.edu.sg>
2019-05-10 13:13:19 +0800

    try layer auto controls

--- tosca/static/facetview/jquery.facetview.js
+++ tosca/static/facetview/jquery.facetview.js
@@ -1661,8 +1661,6 @@ search box - the end user will not know they are happening.
             var osm = L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', { maxZoom: 18 }),
             satview = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', { maxNativeZoom: 18 });
             osm.addTo(location_search_map)
-            satview.addTo(location_search_map)
-
             var baseMaps = {"OSM": osm, "WorldView": satview};
 
             // create the within control
@@ -1683,11 +1681,10 @@ search box - the end user will not know they are happening.
             });
         
             // create product layer
-            prodLayer = L.layerGroup()
-            prodLayer.setZIndex(20)
-            prodLayer.addTo(location_search_map);
+            prodLayer = L.layerGroup().addTo(location_search_map);
+            var prodOverlayMaps = {"Products": prodLayer};
 
-            L.control.layers(baseMaps, {autoZIndex:true}).addTo(location_search_map);
+            L.control.layers(baseMaps, prodOverlayMaps, {autoZIndex:true}).addTo(location_search_map);
 
             // create feature group to hold shapes
             drawnItems = new L.FeatureGroup();
