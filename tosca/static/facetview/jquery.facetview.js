07af0b231b8cc86f43b39f8cb04e79cd428a522e
shitong01 <stchin@ntu.edu.sg>
2019-06-11 16:47:04 +0800

    update facetview maps

--- tosca/static/facetview/jquery.facetview.js
+++ tosca/static/facetview/jquery.facetview.js
@@ -1662,7 +1662,8 @@ search box - the end user will not know they are happening.
             location_search_map = L.map('location_search_map', {
                 worldCopyJump: true
             }).setView([0,0], 1);
-            var baseLayer = L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', { maxZoom: 18 });
+            var baseLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+                maxZoom: 18 });
             baseLayer.addTo(location_search_map);
 
             // create the within control
@@ -1705,7 +1706,7 @@ search box - the end user will not know they are happening.
               }
              else {
               location_search_map.removeLayer(baseLayer)
-              baseLayer = L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', { maxZoom: 18 }).addTo(location_search_map);
+              baseLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', { maxZoom: 18 }).addTo(location_search_map);
               baseLayer.addTo(location_search_map);
               }
               dosearch();
